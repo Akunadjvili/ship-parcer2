d998dac8de82a0e9f3c46f79f569b39aebcb4bc4
Akunadjvili <Akunadjvili@gmail.com>
2021-01-31 19:51:01 +0200

    readme

---
+++ execute.bat
@@ -0,0 +1,2 @@
+node.exe ship-scraper.js --search=DRS --page=50 --limit=0 --type=0 --timeout=5000 --format --hidden=true --dateformat="dd.mm.yyyy"
+pause
--- ship-scraper.js
+++ ship-scraper.js
@@ -8,7 +8,7 @@ const minimist = require("minimist");
 const ProgressBar = require("progress");
 
 const dataJSON = path.resolve(path.dirname(__filename), "result.json");
-const dataTable = path.resolve(path.dirname(__filename), "table.txt");
+const dataTable = path.resolve(path.dirname(__filename), "result.csv");
 const workFiles = [dataJSON, dataTable];
 for (const file of workFiles) {
   if (filesystem.existsSync(file)) {
@@ -27,6 +27,11 @@ const options = buildOptions({
     alias: "s",
     default: "",
   },
+  dateformat: {
+    type: "string",
+    alias: "d",
+    default: "dd.mm.yyyy",
+  },
 
   format: {
     type: "boolean",
@@ -39,20 +44,29 @@ const options = buildOptions({
     alias: "p",
     default: 50,
   },
+
   timeout: {
     type: "number",
-    alias: "t",
-    default: 5000,
+    alias: "tt",
+    default: 30000,
   },
+
   limit: {
     type: "number",
     alias: "l",
     default: 0,
   },
-
+  type: {
+    type: "number",
+    alias: "t",
+    default: 0,
+  },
+  hidden: {
+    type: "boolean",
+    alias: "b",
+    default: true,
+  },
   published: "boolean",
-
-  // Special option for positional arguments (`_` in minimist)
   arguments: "string",
 });
 
@@ -60,34 +74,50 @@ const parseArgs = minimist(process.argv.slice(2), options);
 
 const outputFormat = parseArgs.format;
 const limit = parseArgs.limit;
-const homeURL = "https://www.marinetraffic.com";
-// const searchValue = "DRAWSKO";
+const timeout = parseArgs.timeout;
+const dateformat = parseArgs.dateformat;
 
-let searchValue = parseArgs.search ? parseArgs.search.trim() : "";
-let pagesCount;
-if (parseArgs.page) {
-  pagesCount = Number(parseArgs.page);
-  pagesCount = [10, 20, 50].includes(pagesCount) ? pagesCount : 50;
-} else {
-  pagesCount = 50;
-}
+const browserVisible = parseArgs.hidden;
+
+const type = [0, 1].includes(parseArgs.type) ? parseArgs.type : 0;
+const searchType = type ? `/search_type:${type}` : "";
+
+
+const pagesCount = [10, 20, 50].includes(parseArgs.page) ? parseArgs.page : 50;
+const searchValue = parseArgs.search.trim();
+
+const homeURL = "https://www.marinetraffic.com";
+const cookieModalAcceptSelector =
+  "#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-flk0bs";
+
+// const searchValue = "DRAWSKO";
 
 let browser;
 let page;
 let bar;
 
-const transformDate = (stamp) => {
+const transformDate = (stamp, format = "dd.mm.yyyy") => {
   const date = new Date(stamp * 1000);
-  return dateFormat(date, "dd.mm-yy");
+  return dateFormat(date, format);
 };
-const clearUrl = (url) => {
+
+const clearUrl = (url, base) => {
   const cropIndex = url.indexOf("/mmsi");
   const cropUrl = url.slice(0, cropIndex);
-  return `${homeURL}${cropUrl}`;
+  return `${base}${cropUrl}`;
 };
+
 const getLinks = () => {
   const links = document.querySelectorAll(".search_index_link");
   return Array.from(links, (a) => a.getAttribute("href"));
+};
+
+const saveToFile = (name, data) => {
+  filesystem.writeFile(name, data, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
 const ships = [];
@@ -96,35 +126,25 @@ const saveData = async () => {
   let table = "";
 
   for (const ship of ships) {
-    let name, timestamp, vesselName;
+    let departurePort = "-==missing data==-", timestamp = 0, vesselName = "-==missing data==-";
 
     if (ship.voyageInfo && ship.voyageInfo.departurePort) {
-      name = ship.voyageInfo.departurePort.name;
+      departurePort = ship.voyageInfo.departurePort.name;
       timestamp = ship.voyageInfo.departurePort.timestamp
         ? ship.voyageInfo.departurePort.timestamp
         : 0;
       vesselName = ship.voyageInfo.vesselName;
     } else {
-      name = "платно || нет данных || ошибка";
+      departurePort = "-==missing data==-";
       timestamp = 0;
       if (ship.vesselInfo) {
         vesselName = ship.vesselInfo.name;
       }
     }
-    table += `Vessel: ${vesselName}\tDeparture Port: ${name}\t Date Departure: ${transformDate(
-      timestamp
-    )}\n`;
+    table += `${vesselName};${departurePort};${transformDate(timestamp, dateformat)};\n`;
   }
-  filesystem.writeFile("table.txt", table, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  filesystem.writeFile("result.json", JSON.stringify(ships), function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+  saveToFile("result.csv", table);
+  saveToFile("result.json", JSON.stringify(ships));
 };
 
 const getProgressBar = (line, maximum) => {
@@ -137,52 +157,53 @@ const getProgressBar = (line, maximum) => {
 };
 
 let scrape = async () => {
-  browser = await puppeteer.launch({ headless: true });
+  browser = await puppeteer.launch({ headless: browserVisible });
   page = await browser.newPage();
+  page.setDefaultTimeout(timeout)
+  // page.setDefaultNavigationTimeout(timeout)
   await page.goto(
-    `${homeURL}/en/ais/index/search/all/per_page:${pagesCount}/search_type:1/page:1/keyword:${searchValue}`,
+    `${homeURL}/en/ais/index/search/all/per_page:${pagesCount}${searchType}/page:1/keyword:${searchValue}`,
     { waitUntil: "load" }
   );
 
-  const cookieModalAcceptSelector =
-    "#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-flk0bs";
+
   await page.waitForSelector(cookieModalAcceptSelector);
   await page.click(cookieModalAcceptSelector);
 
-  const count = await page.evaluate(() => {
+  let count = await page.evaluate(() => {
     const span = document.querySelector("#indexForm + span");
-    return span ? span.textContent.match(/\d+/)[0] : 1;
+    return span ? Number(span.textContent.match(/\d+/)[0]) : 1;
   });
 
   bar = getProgressBar(
-    "  Загрузка: Cписок кораблей [:bar] страница [:current из :total]",
-    Number(count)
+    "  Download: List of ships [:bar] page [:current from :total]", Number(count)
   );
   let links = [];
+  console.log(`Found ${count} pages. Processing...`);
   bar.tick(1);
   links = await page.evaluate(getLinks);
   if (count > 1) {
+    count = limit ? Math.min(count, limit) : count
     for (let i = 2; i <= count; i += 1) {
       bar.tick(1);
       await page.goto(
-        `${homeURL}/en/ais/index/search/all/per_page:${pagesCount}/search_type:1/page:${i}/keyword:${searchValue}`,
+        `${homeURL}/en/ais/index/search/all/per_page:${pagesCount}${searchType}/page:${i}/keyword:${searchValue}`,
         { waitUntil: "load" }
       );
       links = [...links, ...(await page.evaluate(getLinks))];
     }
   }
-  links = links.map((elm) => clearUrl(elm));
+  links = links.map((elm) => clearUrl(elm, homeURL));
+  console.log(`Found ${links.length} ships. Downloading info...`);
   if (links.length) {
     for (const ship of links) {
       const id = ship.match(/\d+/)[0];
       const url = ship;
-      const search = `${homeURL}/vesselDetails/voyageInfo/shipid:${id}`;
-      ships.push({ url, search, id });
+      ships.push({ url, id });
     }
 
     bar = getProgressBar(
-      "  Загрузка: корабли [:bar] корабль [:current из :total]",
-      ships.length
+      "  Download: Ships [:bar] ship [:current from :total]", ships.length
     );
 
     for (const ship of ships) {
@@ -191,21 +212,21 @@ let scrape = async () => {
         page
           .waitForResponse((res) => {
             return res.url().includes("/vesselDetails/voyageInfo/shipid:");
-          }, 5000)
+          })
           .then((response) => response.json())
-          .catch((err) => {}),
+          .catch((err) => { }),
         page
           .waitForResponse((res) => {
             return res.url().includes("/vesselDetails/vesselInfo/shipid:");
-          }, 5000)
+          })
           .then((response) => response.json())
-          .catch((err) => {}),
+          .catch((err) => { }),
         page
           .waitForResponse((res) => {
             return res.url().includes("/vesselDetails/latestPosition/shipid:");
-          }, 5000)
+          })
           .then((response) => response.json())
-          .catch((err) => {}),
+          .catch((err) => { }),
         await page.goto(ship.url, { waitUntil: "load" }),
       ]);
       [ship.voyageInfo, ship.vesselInfo, ship.latestPosition] = [
