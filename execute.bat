77e55bc211b1bc0f2ac29a7b42da609695901aa6
Akunadjvili <44133532+Akunadjvili@users.noreply.github.com>
2021-02-02 12:57:32 +0200

    fix command line

--- execute.bat
+++ execute.bat
@@ -1,4 +1,5 @@
-@REM node.exe ship-scraper.js --search=DRS --page=50 --limit=0 --type=0 --timeout=5000 --format --hidden=false --dateformat="dd.mm.yyyy"
-@REM node.exe ship-scraper.js --search=DRAWSKO --page=50 --limit=0 --type=0 --timeout=5000 --format --hidden=false --dateformat="dd.mm.yyyy"
-node.exe ship-scraper.js --search=HHH --page=10 --limit=1 --type=1 --timeout=5000 --format --hidden=true --dateformat="dd.mm.yyyy"
+@REM первый корабль
+node.exe ship-scraper.js --search="Nu Sakura" --page=50 --limit=0 --type=0 --timeout=5000 --hidden=true --dateformat="dd.mm.yyyy"
+@REM второй корабль
+node.exe ship-scraper.js --search="Next Name" --page=50 --limit=0 --type=0 --timeout=5000 --hidden=true --dateformat="dd.mm.yyyy"
 pause
